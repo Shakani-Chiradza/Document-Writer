95d506eab3c3e7cb61051962faa5dbe422327661
Shakani-Chiradza <shakanichiradza@gmail.com>
2023-09-02 18:52:12 +0200

    Add font options features

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 var letterCount = 0
-var countColors = 0
+var countBgColors = 0
+var countFontsUp = 0
+var countFontsDown = 0
+var countFontColors = 0
 
 function checkOverflow(){
 
@@ -18,9 +21,9 @@ function checkOverflow(){
     }
 
     
-    if (document.getElementById("blank-page").scrollHeight != 773){
+    if (document.getElementById("blank-page").scrollHeight != 1051){
         document.getElementById("blank-page1").style.display = 'block'
-        document.getElementById("blank-page").scrollHeight == 773
+        document.getElementById("blank-page").scrollHeight == 1051
        
         if (this.event.key != 'Backspace'){
                 { document.getElementById("blank-page1").focus()}
@@ -49,6 +52,7 @@ function clickedMenuItem(id) {
     }
 
    // Code for dropping down menu items for navbar items with submenus
+   // NB: implement map instead of for loops
 
    let fileOptions = document.getElementsByClassName('file-options-items')
    let insertItems = document.getElementsByClassName('insert-items')
@@ -170,16 +174,76 @@ function changeBackground() {
     const bgColors = ['lightyellow', 'lightgreen', 'pink', 'lightBlue', 'white']
 
     for (let i = 0; i < blankPage.length; i++){
-        if (countColors < 5) {
-        blankPage[i].style.backgroundColor = bgColors[countColors]
+        if (countBgColors < 5) {
+        blankPage[i].style.backgroundColor = bgColors[countBgColors]
         }
         else {
-            let manyColors = countColors % 5
+            let manyColors = countBgColors % 5
             blankPage[i].style.backgroundColor = bgColors[manyColors]
         }
     }
 
-    countColors += 1
+    countBgColors += 1
+
+}
 
 
+function bold() {
+    var pageContent = document.getElementById("blank-page")
+    pageContent.style.fontWeight = 'bold'
+}
+
+function italic() {
+    var pageContent = document.getElementById("blank-page")
+    pageContent.style.fontStyle = 'italic'
+}
+
+function underline() {
+    var pageContent = document.getElementById("blank-page")
+    pageContent.style.textDecoration = 'underline'
+}
+
+function increaseFont() {
+    var pageContent = document.getElementById("blank-page")
+    const fontPixels = ['30px', '35px', '40px', '45px']
+    if(countFontsUp < 4) {
+        pageContent.style.fontSize = fontPixels[countFontsUp]
+    }
+    else{
+        let manyFonts = countFontsUp % 4
+        pageContent.style.fontSize = fontPixels[manyFonts]
+    }
+
+    countFontsUp += 1
+    
+}
+
+function decreaseFont() {
+    var pageContent = document.getElementById("blank-page")
+    console.log(pageContent)
+    const fontPixels = ['20px', '15px', '10px', '5px']
+    if(countFontsDown < 4) {
+        pageContent.style.fontSize = fontPixels[countFontsDown]
+        
+    }
+    else{
+        let manyFonts = countFontsDown % 4
+        pageContent.style.fontSize = fontPixels[manyFonts]
+    }
+
+    countFontsDown += 1
+}
+
+function changeFontColor() {
+    var pageContent = document.getElementById("blank-page")
+    const fontColors = ['blue', 'red', 'purple', 'orange', 'black']
+    if(countFontColors < 5){
+        pageContent.style.color = fontColors[countFontColors]
+    }
+    else{
+        let manyColors = countFontColors % 5
+        pageContent.style.color = fontColors[manyColors]
+    }
+
+    countFontColors += 1
 }
